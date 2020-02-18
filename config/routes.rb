1ae29e1b0167576f1d1caeee95210b1952a38bdb
Sujal <ssujal@ncsu.edu>
2020-02-17 19:06:16 -0500

    view users + delete users + not admin deletion + partial user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
-Rails.application.routes.draw do
-  # Routing to HomePage
-  root to: 'welcome#index'
-
+Rails.application.routes.draw do  
   # routing devise to use our custom registrations controller
   devise_for :users, :controllers => { registrations: 'registrations' }
-  
+
+  resources :users
+
   resources :items do
     resources :reviews, except: [:show, :index]
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Routing to HomePage
+  root to: 'welcome#index'
 end
