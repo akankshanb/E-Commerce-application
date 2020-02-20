0db398662a28d485a763b20052680d009c6b75e4
Sujal <ssujal@ncsu.edu>
2020-02-19 21:30:31 -0500

    cleanup (basic sign up + admin login) (no admin CRUD)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,26 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
-    before_action :authenticate_user!, :redirect_unless_admin,  only: [:new, :create]
-    skip_before_action :require_no_authentication
+    # before_action :authenticate_user!, only: [:new, :create]
+    # skip_before_action :require_no_authentication
+    # prepend_before_action :require_no_authentication, only: :cancel
 
     private
 
-    def redirect_unless_admin
-      unless current_user.try(:admin?)
-        flash[:error] = "Only admins can do that"
-        redirect_to root_path
-      end
-    end
+    # def redirect_unless_admin
+    #   unless current_user.try(:admin?)
+    #     flash[:error] = "Only admins can do that"
+    #     redirect_to root_path
+    #   end
+    # end
   
-    def sign_up(resource_name, resource)
-      true
-    end
+    # def sign_up(resource_name, resource)
+    #   true
+    # end
     
     def sign_up_params
       params.require(:user).permit(:name, :phone, :dob, :address, :email, :password, :password_confirmation, :card_name, :card_number, :exp, :cvv)
     end
   
     def account_update_params
-      params.require(:user).permit(:name, :dob, :email, :password, :password_confirmation, :current_password)
+      params.require(:user).permit(:name, :phone, :dob, :address, :email, :password, :password_confirmation, :card_name, :card_number, :exp, :cvv)
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do  
   # routing devise to use our custom registrations controller
   devise_for :users, :controllers => { registrations: 'registrations' }
-
+  
   resources :users
-
+  
   resources :items do
     resources :reviews, except: [:show, :index]
   end
