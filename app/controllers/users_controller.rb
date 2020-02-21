591a6c27728d6b0380c948b8aa9ff727945abb88
Sujal <ssujal@ncsu.edu>
2020-02-21 02:11:14 -0500

    restrict /users_admin from non-admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :protect, only: [:index]
     before_action :set_user, only: [:show, :edit, :destroy, :update]
     
     # GET /users
@@ -38,17 +39,17 @@ class UsersController < ApplicationController
 
     # PATCH/PUT /users/1
     # PATCH/PUT /users/1.json
-    # def update
-    #     respond_to do |format|
-    #     if @user.update(user_params)
-    #         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #         format.json { render :show, status: :ok, location: @user }
-    #     else
-    #         format.html { render :edit }
-    #         format.json { render json: @user.errors, status: :unprocessable_entity }
-    #     end
-    #     end
-    # end
+    def update
+        respond_to do |format|
+        if @user.update(user_params)
+            format.html { redirect_to @user, notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
+        else
+            format.html { render :edit }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+        end
+    end
 
     # DELETE /users/1
     # DELETE /users/1.json
@@ -68,5 +69,11 @@ class UsersController < ApplicationController
 
     def user_params
         params.permit(:name, :phone, :dob, :address, :email, :password, :password_confirmation, :card_name, :card_number, :exp, :cvv)
+    end
+
+    def protect
+        if current_user.admin == false
+            redirect_to root_path, notice: "You do not have admin priviledges!"
+        end
     end
 end
