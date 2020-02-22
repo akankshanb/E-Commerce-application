f9516176497bec5c620b9567daed1d888caabe9f
Sujal <ssujal@ncsu.edu>
2020-02-21 23:33:54 -0500

    removed seed, smtp prod

--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 2020_02_20_204453) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.float "popularity"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -68,23 +68,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_204453) do
     t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["item_id"], name: "index_line_items_on_item_id"
-  end
-
-  create_table "order_items", force: :cascade do |t|
-    t.integer "quantity"
-    t.integer "item_id"
-    t.integer "order_id"
-    t.decimal "unit_price"
-    t.decimal "total"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.decimal "subtotal"
-    t.decimal "total"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -121,6 +104,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_204453) do
     t.string "card_number"
     t.date "exp"
     t.integer "cvv"
+    t.string "otp_secret_key"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Item.create!([
-                 {brand: "Strawberry Canyon LLC", name: "Saas Textbook", category: "Books", quantity: 30, cost: 26.25, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg", popularity: 0.0},
-                 {brand: "Grandma cooks", name: "Naan", category: "Grocery", quantity: 10, cost: 2.1, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/Naan.jpg", popularity: 0.384},
-                 {brand: "Apple", name: "Macbook", category: "Electronics", quantity: 10, cost: 2875.0, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Macbook.jpg", popularity: 0.0},
-                 {brand: "Buy your Luck", name: "Silver Lucky Charm", category: "Jewellery", quantity: 20, cost: 10.5, purchases: 0, available: true, special: false, restricted: false, age_restricted: true, image: "./assets/images/saas_book.jpg/silver-lucky-charm.jpg", popularity: 0.0},
-                 {brand: "Apple", name: "Iphone 11", category: "Electronics", quantity: 10, cost: 1150.0, purchases: 0, available: true, special: true, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Apple_iPhone_11.png", popularity: 0.408},
-                 {brand: "Mush Makers", name: "Blender 2.0", category: "Electronics", quantity: 0, cost: 31.5, purchases: 0, available: false, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Blender.jfif", popularity: 0.0}
-             ])
-Review.create!([
-                   {rating: 5, comment: "Amazing phone! I love it!", item_id: 3},
-                   {rating: 2, comment: "Very fragile. Broke it thrice already! Iphone? More like an Icephone!", item_id: 3},
-                   {rating: 4, comment: "I wanna give 10/5 but can't!", item_id: 3},
-                   {rating: 1, comment: "Android Forever!!!", item_id: 3},
-                   {rating: 5, comment: "Just like my grandma makes!", item_id: 4},
-                   {rating: 3, comment: "I microwaved them. They are soggy now.", item_id: 4},
-                   {rating: 2, comment: "Tastes like eating a towel- wet, rough and smelly.", item_id: 4},
-                   {rating: 1, comment: "Grandpa rules!!!", item_id: 4},
-                   {rating: 5, comment: "Grandma rocks!", item_id: 4},
-                   {rating: 5, comment: "Amazing phone!", item_id: 3}
-               ])
-Subscribe.create!([
-                      {item_id: 11, user_id: 1}
-                  ])
+# Item.create!([
+#                  {brand: "Strawberry Canyon LLC", name: "Saas Textbook", category: "Books", quantity: 30, cost: 26.25, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg", popularity: 0.0},
+#                  {brand: "Grandma cooks", name: "Naan", category: "Grocery", quantity: 10, cost: 2.1, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/Naan.jpg", popularity: 0.384},
+#                  {brand: "Apple", name: "Macbook", category: "Electronics", quantity: 10, cost: 2875.0, purchases: 0, available: true, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Macbook.jpg", popularity: 0.0},
+#                  {brand: "Buy your Luck", name: "Silver Lucky Charm", category: "Jewellery", quantity: 20, cost: 10.5, purchases: 0, available: true, special: false, restricted: false, age_restricted: true, image: "./assets/images/saas_book.jpg/silver-lucky-charm.jpg", popularity: 0.0},
+#                  {brand: "Apple", name: "Iphone 11", category: "Electronics", quantity: 10, cost: 1150.0, purchases: 0, available: true, special: true, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Apple_iPhone_11.png", popularity: 0.408},
+#                  {brand: "Mush Makers", name: "Blender 2.0", category: "Electronics", quantity: 0, cost: 31.5, purchases: 0, available: false, special: false, restricted: false, age_restricted: false, image: "./assets/images/saas_book.jpg/Blender.jfif", popularity: 0.0}
+#              ])
+# Review.create!([
+#                    {rating: 5, comment: "Amazing phone! I love it!", item_id: 3},
+#                    {rating: 2, comment: "Very fragile. Broke it thrice already! Iphone? More like an Icephone!", item_id: 3},
+#                    {rating: 4, comment: "I wanna give 10/5 but can't!", item_id: 3},
+#                    {rating: 1, comment: "Android Forever!!!", item_id: 3},
+#                    {rating: 5, comment: "Just like my grandma makes!", item_id: 4},
+#                    {rating: 3, comment: "I microwaved them. They are soggy now.", item_id: 4},
+#                    {rating: 2, comment: "Tastes like eating a towel- wet, rough and smelly.", item_id: 4},
+#                    {rating: 1, comment: "Grandpa rules!!!", item_id: 4},
+#                    {rating: 5, comment: "Grandma rocks!", item_id: 4},
+#                    {rating: 5, comment: "Amazing phone!", item_id: 3}
+#                ])
+# Subscribe.create!([
+#                       {item_id: 11, user_id: 1}
+#                   ])
