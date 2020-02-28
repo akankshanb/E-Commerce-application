8245cbcf70551baea0c11899fb46fd6e7148d3f7
vpisapa <vpisapa@ncsu.edu>
2020-02-28 14:33:15 -0500

    enumerator for status column in Order implemented

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  # adding an enum for the column status
+  enum status: { 'Purchased': 0, 'Pending Admin Approval': 1, 'Special Item Purchased': 2, 'Request rejected - Refunded': 3, 'Return Requested - awaiting Admin approval': 4, 'Return Approved': 5, 'Return Rejected': 6}
 
   # entering the line items into the order table
   def self.enter_line_items(user, cart)
